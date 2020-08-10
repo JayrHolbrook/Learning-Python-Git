8ba71cef551106cb64dea96c4a4069caa473a7e0
Jaden Holbrook <JayRHolbrook@Jadens-MacBook-Pro.local>
2020-08-09 22:00:48 -0500

    First Lesson

--- test2.py
+++ test2.py
@@ -1,1 +1,7 @@
-print("hello Wrld")
+print "hello Wrld"
+print "Hello Again"
+print "I like typing this"
+print "this is fun"
+print 'Yay! Printing.'
+print "i'd much rather you 'not'"
+print 'I "said" do not touch this.'
