e4f0a6c7ec09cf0408fcaa0b143da2d26843a492
JayRHolbrook <JayRHolbrook@Jadens-MacBook-Pro.local>
2020-08-14 16:42:33 -0500

    Changing to Python 3

--- EX1.py
+++ EX1.py
@@ -1,7 +1,7 @@
-print "hello Wrld"
+print ("hello Wrld")
 #print "Hello Again"
-print "I like typing this."
+print ("I like typing this.")
 #print "this is fun"
-print 'Yay! Printing.'
+print ('Yay! Printing.')
 #print "i'd much rather you 'not'"
-print 'I "said" do not touch this.'
+print ('I "said" do not touch this.')
