6f4add47fca040c137e4ecf189da1ee680eace02
Jaden Holbrook <JayRHolbrook@Jadens-MacBook-Pro.local>
2020-08-13 21:06:25 -0500

    Comment studying

--- test2.py
+++ test2.py
@@ -1,7 +1,7 @@
 print "hello Wrld"
-print "Hello Again"
+#print "Hello Again"
 print "I like typing this."
-print "this is fun"
+#print "this is fun"
 print 'Yay! Printing.'
-print "i'd much rather you 'not'"
+#print "i'd much rather you 'not'"
 print 'I "said" do not touch this.'
