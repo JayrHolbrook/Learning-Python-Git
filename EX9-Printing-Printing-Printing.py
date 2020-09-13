2b04aa190424dd10c49283104830f2e92f216066
Jaden Holbrook <Jaden.r.holbrook@gmail.com>
2020-09-13 16:48:10 -0500

    fix

--- EX9-Printing-Printing-Printing.py
+++ EX9-Printing-Printing-Printing.py
@@ -10,5 +10,5 @@ print("""
 There's something going on here.
 with the three double-qoutes.
 we'll be able to type as much as we like.
-Even 4 lines if we want, or5, or 6.
+Even 4 lines if we want, or 5, or 6.
 """)
